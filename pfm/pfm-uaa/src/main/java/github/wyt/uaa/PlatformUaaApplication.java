3b9ecb02b2bb1f0cf5c6c5c2a76158850fdae8d4
wangyongtao <wangyt4dev@gmail.com>
2018-08-09 13:20:07 +0800

    Dockerfile & docker compose.

--- pfm/pfm-uaa/src/main/java/github/wyt/uaa/PlatformUaaApplication.java
+++ pfm/pfm-uaa/src/main/java/github/wyt/uaa/PlatformUaaApplication.java
@@ -2,12 +2,14 @@ package github.wyt.uaa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 /**
  * @author wangyongtao
  * @date 2018/7/18
  */
 @SpringBootApplication
+@EnableEurekaClient
 public class PlatformUaaApplication {
 
   public static void main(String[] args) {
