a2dd89d6ffd0562dbc6c1028acf4fe744b635830
wangyongtao <wangyt4dev@gmail.com>
2018-12-18 14:06:42 +0800

    eureka server按照文档做修改.

--- infra/eureka-server/src/main/java/github/wyt/eureka/EurekaServerApplication.java
+++ infra/eureka-server/src/main/java/github/wyt/eureka/EurekaServerApplication.java
@@ -1,7 +1,7 @@
 package github.wyt.eureka;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
@@ -14,7 +14,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 public class EurekaServerApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(EurekaServerApplication.class, args);
-  }
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaServerApplication.class).web(true).run(args);
+    }
 }
