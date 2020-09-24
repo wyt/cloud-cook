88bbc71142c1748066963a99b9eaceef614670d2
wangyongtao <wangyt4dev@gmail.com>
2020-09-24 15:51:09 +0800

    集成Nacos改造.

--- app/app-sample/app-sample-consume/src/main/java/io/mysnippet/consume/SampleConsumeApplication.java
+++ app/app-sample/app-sample-consume/src/main/java/io/mysnippet/consume/SampleConsumeApplication.java
@@ -2,7 +2,7 @@ package io.mysnippet.consume;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -13,7 +13,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @date 2018/7/16
  */
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients // 开启Feign Client功能
 @EnableHystrix // 开启熔断器功能
 public class SampleConsumeApplication {
