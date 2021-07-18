c9aa4159206cbb17d65bf9e4813e3f6a4304d271
wangyongtao <wangyt4dev@gmail.com>
2021-07-18 16:05:16 +0800

    sentinel_v3

--- infra/gateway-service/src/main/java/github/wyt/gateway/custom/BlockResponse.java
+++ infra/gateway-service/src/main/java/github/wyt/gateway/custom/BlockResponse.java
@@ -1,9 +1,30 @@
 package github.wyt.gateway.custom;
 
+/**
+ * 重写BlockResponse,自定义响应体
+ *
+ * @author wangyongtao
+ * @date 2021-7-18
+ */
 public class BlockResponse
     extends com.alibaba.csp.sentinel.adapter.gateway.zuul.fallback.BlockResponse {
 
+  private String respBody;
+
   public BlockResponse(int code, String message, String route) {
     super(code, message, route);
+  }
+
+  @Override
+  public String toString() {
+    return respBody;
+  }
+
+  public String getRespBody() {
+    return respBody;
+  }
+
+  public void setRespBody(String respBody) {
+    this.respBody = respBody;
   }
 }
--- infra/gateway-service/src/main/java/github/wyt/gateway/fallback/ZuulFallbackProvider.java
+++ infra/gateway-service/src/main/java/github/wyt/gateway/fallback/ZuulFallbackProvider.java
@@ -1,9 +1,9 @@
 package github.wyt.gateway.fallback;
 
-import com.alibaba.csp.sentinel.adapter.gateway.zuul.fallback.BlockResponse;
 import com.alibaba.csp.sentinel.adapter.gateway.zuul.fallback.ZuulBlockFallbackProvider;
 import com.alibaba.csp.sentinel.log.RecordLog;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
+import github.wyt.gateway.custom.BlockResponse;
 
 public class ZuulFallbackProvider implements ZuulBlockFallbackProvider {
 
@@ -14,12 +14,31 @@ public class ZuulFallbackProvider implements ZuulBlockFallbackProvider {
 
   @Override
   public BlockResponse fallbackResponse(String route, Throwable cause) {
+
     RecordLog.info(
         String.format("[Sentinel DefaultBlockFallbackProvider] Run fallback route: %s", route));
+
     if (cause instanceof BlockException) {
-      return new BlockResponse(429, "Sentinel block exception", route);
+      BlockResponse blockResponse = new BlockResponse(200, null, route);
+      String respBody = "{\"code\":\"200\",\"msg\":\"哥们儿我被限流了\"}";
+      blockResponse.setRespBody(respBody);
+      return blockResponse;
     } else {
-      return new BlockResponse(500, "System Error", route);
+      BlockResponse blockResponse = new BlockResponse(500, null, route);
+      String respBody = "{\"status\":\"11004\",\"message\":\"网络开小差了,请稍后重试\"}";
+      blockResponse.setRespBody(respBody);
+      return blockResponse;
     }
   }
+
+  //  @Override
+  //  public BlockResponse fallbackResponse(String route, Throwable cause) {
+  //    RecordLog.info(
+  //        String.format("[Sentinel DefaultBlockFallbackProvider] Run fallback route: %s", route));
+  //    if (cause instanceof BlockException) {
+  //      return new BlockResponse(429, "Sentinel block exception", route);
+  //    } else {
+  //      return new BlockResponse(500, "System Error", route);
+  //    }
+  //  }
 }
